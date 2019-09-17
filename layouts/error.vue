4697b7dd1ae354c4b656ffc15c1b191676370552
ldesgoui <ldesgoui@gmail.com>
2019-09-17 20:50:35 +0200

    error page style

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,15 +1,19 @@
 <template>
-  <div>
-    <h1 v-if="error.statusCode === 404">
+  <div class="max-w-md mx-auto p-8 text-center font-sans">
+    <div v-if="error.statusCode === 404">
       <p>Ruh roh, gamer dude, you've taken a wrong turn!</p>
       <p>This ain't what you want, this is the ol' zany...</p>
-      <p>Page Not Found</p>
-    </h1>
-    <h1 v-else>
+      <p class="font-bold">Page Not Found</p>
+    </div>
+    <div v-else>
       <p>Omega woops!</p>
-      <p>The website just dun done broke!</p>
+      <p class="font-bold">The website just dun done broke!</p>
       <p>Vewy sowwy</p>
-    </h1>
+    </div>
+
+    <n-link class="mt-8" to="/">
+      to Homepage
+    </n-link>
   </div>
 </template>
 
